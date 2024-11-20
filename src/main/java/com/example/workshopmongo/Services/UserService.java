e709c872954cb1710572480261a77a8b5dbe47d5
Willian Dias <williandias01@gmail.com>
2024-11-20 12:09:08 -0300

    Inserção de usuário com POST

--- src/main/java/com/example/workshopmongo/Services/UserService.java
+++ src/main/java/com/example/workshopmongo/Services/UserService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.workshopmongo.DTOs.UserDTO;
 import com.example.workshopmongo.Domain.User;
 import com.example.workshopmongo.Repository.UserRepository;
 import com.example.workshopmongo.Services.Exceptions.ObjectNotFoundException;
@@ -15,13 +16,21 @@ public class UserService {
 
 	@Autowired
 	private UserRepository userRepository;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return userRepository.findAll();
 	}
-	
+
 	public User findById(String id) {
 		Optional<User> user = userRepository.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+	public User Insert(User obj) {
+		return userRepository.insert(obj);
+	}
+
+	public User FromDTO(UserDTO userDTO) {
+		return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
 	}
 }
--- src/main/java/com/example/workshopmongo/resources/UserResource.java
+++ src/main/java/com/example/workshopmongo/resources/UserResource.java
@@ -1,14 +1,17 @@
 package com.example.workshopmongo.Resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.workshopmongo.DTOs.UserDTO;
 import com.example.workshopmongo.Domain.User;
@@ -33,4 +36,13 @@ public class UserResource {
 		User user = userService.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(user));
 	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> Insert(@RequestBody UserDTO userDTO) {
+		User user = userService.FromDTO(userDTO);
+		user = userService.Insert(user);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+
 }
