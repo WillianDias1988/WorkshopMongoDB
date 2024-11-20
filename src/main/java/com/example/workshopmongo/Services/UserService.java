6ce277c42f713c71285b508f365839bbf71640b6
Willian Dias <williandias01@gmail.com>
2024-11-20 11:46:39 -0300

    Obtendo um usuário por id

--- src/main/java/com/example/workshopmongo/Services/UserService.java
+++ src/main/java/com/example/workshopmongo/Services/UserService.java
@@ -1,19 +1,27 @@
 package com.example.workshopmongo.Services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.workshopmongo.Domain.User;
 import com.example.workshopmongo.Repository.UserRepository;
+import com.example.workshopmongo.Services.Exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
 
 	@Autowired
 	private UserRepository userRepository;
+	
 	public List<User> findAll(){
 		return userRepository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> user = userRepository.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
--- src/main/java/com/example/workshopmongo/resources/UserResource.java
+++ src/main/java/com/example/workshopmongo/resources/UserResource.java
@@ -5,8 +5,9 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.workshopmongo.DTOs.UserDTO;
@@ -20,11 +21,16 @@ public class UserResource {
 	@Autowired
 	private UserService userService;
 
-	@GetMapping
+	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> list = userService.findAll();
 		List<UserDTO> listUserDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listUserDTO);
 	}
 
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+		User user = userService.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(user));
+	}
 }
